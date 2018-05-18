81b814cae3976e21a42b501a5cdaaebc1a82126d
Byblik272 <bouth@mail.ru>
2018-05-18 20:53:43 +0300

    clean code

--- src/main/java/project/service/UserDetailsServiceImpl.java
+++ src/main/java/project/service/UserDetailsServiceImpl.java
@@ -13,7 +13,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-//@Service(value = "userService")
+@Service(value = "userService")
 public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Autowired UserService userService;
--- src/main/java/project/service/UserServiceImpl.java
+++ src/main/java/project/service/UserServiceImpl.java
@@ -17,23 +17,10 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-@Service(value = "userService")
-public class UserServiceImpl implements UserDetailsService, UserService {
+@Service
+public class UserServiceImpl implements UserService {
 
     @Autowired private UserRepository userRepository;
-
-    @Override
-    public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
-        UserDTO user = userRepository.getUserByLogin(login);
-        if(user == null){
-            throw new UsernameNotFoundException("Invalid username or password.");
-        }
-        return new User(user.getLogin(), user.getPassword(), getAuthority());
-    }
-
-    private List<SimpleGrantedAuthority> getAuthority() {
-        return Collections.singletonList(new SimpleGrantedAuthority("ROLE_ADMIN"));
-    }
 
     @Override
     public UserDTO getUserByLogin(String login) {
