caa75d82ac73e7a04b77f74c89b7c8313178a6f4
Byblik272 <bouth@mail.ru>
2018-05-18 20:54:29 +0300

    clean code

--- src/main/java/project/service/UserServiceImpl.java
+++ src/main/java/project/service/UserServiceImpl.java
@@ -3,18 +3,11 @@ package project.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import project.dto.UserDTO;
 import project.repository.UserRepository;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 @Service
